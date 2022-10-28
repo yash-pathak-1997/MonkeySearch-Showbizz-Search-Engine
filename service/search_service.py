70e0dd7d3be6d6cda6a6b71bbc2d7971a6ebcc58
krati <kratiagrawal100@gmail.com>
2022-10-28 10:50:04 +0530

    search in one function none type handled

--- service/search_service.py
+++ service/search_service.py
@@ -3,77 +3,75 @@ import requests
 import pandas as pd
 import json
 
-list_type=['movie','tv']
-list_type2=['celebrity','franchise']
+list_type = ['movie', 'tv', 'celebrity', 'franchise']
+list_type2 = ['celebrity', 'franchise']
+base_uri = "https://www.rottentomatoes.com/search?search="
 
 
 def search_service(keyw, filter_data):
-    result_data=pd.DataFrame()
+    result_data = pd.DataFrame()
     if f'{filter_data["type"]}' == '':
         for i in list_type:
-            data=search_data(keyw,i)
-            result_data=pd.concat([result_data,data.head(3)],ignore_index=True)
-        for i in list_type2:
-            data = search_celebrity(keyw,i)
+            data = search_data(keyw, i)
             result_data = pd.concat([result_data, data.head(3)], ignore_index=True)
-
-
     else:
-        data= search_data(keyw,f'{filter_data["type"]}')
+        data = search_data(keyw, f'{filter_data["type"]}')
         # print("search_services")
         # print(result_data)
-        result_data=data
+        result_data = data
         # print(result_data)
     print(len(result_data))
     return json.loads(result_data.to_json(orient="records"))
 
 
-def search_celebrity(keyw,category):
-    url = f"https://www.rottentomatoes.com/search?search={keyw}"
+'''def search_celebrity(keyw,category):
+    url = f"{base_uri}{keyw}"
+    movie_name = []
+    year = []
+    cast = []
+    image = []
+    movie_link = []
+    f_cast = list()
     # print(url)
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    scrap_data_movie = soup.find('search-page-result', type=f'{category}').find('ul').find_all('search-page-item-row')
+    scrap_data_movie = soup.find('search-page-result', type=f'{category}')
     print("scrapdata", scrap_data_movie)
-    movie_name = []
-    year = []
-    cast = []
-    image = []
-    movie_link = []
+    if scrap_data_movie is not None:
+        scrap_data_movie=scrap_data_movie.find('ul').find_all('search-page-item-row')
+        for s in scrap_data_movie:
+            y = s.get('releaseyear')  # release year
+            print(y)
+            if y is None or y.isspace() or not y:
+                y = 'NA'
+            year.append(y)
+            c = s.get('cast')  # cast of movie
+            if c is None or c.isspace() or not c:
+                c = 'NA'
+            cast.append(c)
+            ds = s.find_all('a', class_="unset")
+            p0 = ds[0].find('img')  # image tag of movie
+            img = p0.get('src')
+            if img is None or img.isspace() or not img:
+                img = 'NA'
+            image.append(img)
+            p1 = ds[1].text.strip()  # name of movie
+            mlink = ds[1].get('href')
+            if mlink is None or mlink.isspace() or not mlink:
+                mlink = 'NA'
+            movie_link.append(mlink)
+            if p1 is None or p1.isspace() or not p1:
+                p1 = 'NA'
+            movie_name.append(p1)
 
-    for s in scrap_data_movie:
-        y = s.get('releaseyear')  # release year
-        print(y)
-        if y is None or y.isspace() or not y:
-            y = 'NA'
-        year.append(y)
-        c = s.get('cast')  # cast of movie
-        if c is None or c.isspace() or not c:
-            c = 'NA'
-        cast.append(c)
-        ds = s.find_all('a', class_="unset")
-        p0 = ds[0].find('img')  # image tag of movie
-        img = p0.get('src')
-        if img is None or img.isspace() or not img:
-            img = 'NA'
-        image.append(img)
-        p1 = ds[1].text.strip()  # name of movie
-        mlink = ds[1].get('href')
-        if mlink is None or mlink.isspace() or not mlink:
-            mlink = 'NA'
-        movie_link.append(mlink)
-        if p1 is None or p1.isspace() or not p1:
-            p1 = 'NA'
-        movie_name.append(p1)
+        # preprocess cast
+        print(cast)
 
-    # preprocess cast
-    print(cast)
-    f_cast = list()
-    for entry in cast:
-        f_cast.append(str(entry).split(","))
+        for entry in cast:
+            f_cast.append(str(entry).split(","))
 
-    print(type(image))
-    print(image)
+        print(type(image))
+        print(image)
 
     data = pd.DataFrame()
     data['mname'] = movie_name
@@ -87,52 +85,60 @@ def search_celebrity(keyw,category):
     # js = data.to_json(orient="records")
     # print(js)
     # return js
+'''
 
-def search_data(keyw,category):
-    url = f"https://www.rottentomatoes.com/search?search={keyw}"
+
+def search_data(keyw, category):
+    url = f"{base_uri}{keyw}"
+    print("url is", url)
+    movie_name = []
+    year = []
+    cast = []
+    image = []
+    movie_link = []
+    f_cast = list()
     # print(url)
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
-    scrap_data_movie = soup.find('search-page-result', type=f'{category}').find('ul').find_all('search-page-media-row')
-    print("scrapdata",scrap_data_movie)
-    movie_name = []
-    year = []
-    cast = []
-    image = []
-    movie_link = []
+    scrap_data_movie = soup.find('search-page-result', type=f'{category}')
+    print("scrapdata", scrap_data_movie)
+    if scrap_data_movie is not None:
+        if f'{category}' not in list_type2:
+            scrap_data_movie = scrap_data_movie.find('ul').find_all('search-page-media-row')
+        else:
+            scrap_data_movie = scrap_data_movie.find('ul').find_all('search-page-item-row')
 
-    for s in scrap_data_movie:
-        y = s.get('releaseyear')  # release year
-        if y is None or y.isspace() or not y:
-            y='NA'
-        year.append(y)
-        c = s.get('cast')  # cast of movie
-        if c is None or c.isspace() or not c:
-            c='NA'
-        cast.append(c)
-        ds = s.find_all('a', class_="unset")
-        p0 = ds[0].find('img')  # image tag of movie
-        img=p0.get('src')
-        if img is None or img.isspace() or not img:
-            img='NA'
-        image.append(img)
-        p1 = ds[1].text.strip()  # name of movie
-        mlink = ds[1].get('href')
-        if mlink is None or mlink.isspace() or not mlink:
-            mlink='NA'
-        movie_link.append(mlink)
-        if p1 is None or p1.isspace() or not p1:
-            p1='NA'
-        movie_name.append(p1)
+        for s in scrap_data_movie:
+            y = s.get('releaseyear')  # release year
+            if y is None or y.isspace() or not y:
+                y = 'NA'
+            year.append(y)
+            c = s.get('cast')  # cast of movie
+            if c is None or c.isspace() or not c:
+                c = 'NA'
+            cast.append(c)
+            ds = s.find_all('a', class_="unset")
+            p0 = ds[0].find('img')  # image tag of movie
+            img = p0.get('src')
+            if img is None or img.isspace() or not img:
+                img = 'NA'
+            image.append(img)
+            p1 = ds[1].text.strip()  # name of movie
+            mlink = ds[1].get('href')
+            if mlink is None or mlink.isspace() or not mlink:
+                mlink = 'NA'
+            movie_link.append(mlink)
+            if p1 is None or p1.isspace() or not p1:
+                p1 = 'NA'
+            movie_name.append(p1)
 
-    # preprocess cast
-    print(cast)
-    f_cast = list()
-    for entry in cast:
-        f_cast.append(str(entry).split(","))
+        # preprocess cast
+        print(cast)
+        for entry in cast:
+            f_cast.append(str(entry).split(","))
 
-    print(type(image))
-    print(image)
+        print(type(image))
+        print(image)
 
     data = pd.DataFrame()
     data['mname'] = movie_name
@@ -146,4 +152,3 @@ def search_data(keyw,category):
     # js = data.to_json(orient="records")
     # print(js)
     # return js
-
