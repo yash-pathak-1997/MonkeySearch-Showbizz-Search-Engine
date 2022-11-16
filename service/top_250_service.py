14547dd13d7bae4b3eb32c56736adabcdb259604
krati <kratiagrawal100@gmail.com>
2022-11-16 13:43:23 +0530

    top 250 same poster resolved

--- service/top_250_service.py
+++ service/top_250_service.py
@@ -20,27 +20,39 @@ def top_250_service(sort_by, order):
         rates = []
         years = []
         for s in scrap_table:
-            links.append(s.find('td', class_='titleColumn').find_all('a',
+            # links.append(s.find('td', class_='titleColumn').find_all('a',
+            #                                                          attrs={'href': re.compile("^/title/")}))
+
+            links.append(s.find('td', class_='posterColumn').find_all('a',
                                                                      attrs={'href': re.compile("^/title/")}))
             movies.append(s.find('td', class_='titleColumn').a.text)
             rates.append(s.find('td', class_='ratingColumn').text.replace('\n', ""))
             years.append(s.find('span', class_='secondaryInfo').text.replace('(', "").replace(')', ''))
-
+        # print(links)
         data = pd.DataFrame()
         data['movies'] = movies
         data['rating'] = rates
         data['year'] = years
         link_title = []
-        scrap_movies = soup.find_all('td', class_='titleColumn')
+        scrap_movies = soup.find_all('td', class_='posterColumn')
         img_src = []
+        # print("..........................................")
+        # print(scrap_movies)
+        # print("******************************************")
         for link in scrap_movies:
-            p = s.find('a', attrs={'href': re.compile("^/title/")})
-            i = s.find('img')
+            # print(link)
+            p = link.find('a', attrs={'href': re.compile("^/title/")})
+            i =link.find('img')
             link_title.append(link_base+p.get('href'))
             img_src.append(i.get('src'))
+            print(i)
 
         data['alink'] = link_title
         data['imgsrc'] = img_src
         return json.loads(data.to_json(orient="records"))
     except Exception as e:
         print(e)
+
+#
+# https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY67_CR0,0,45,67_AL_.jpg
+# https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY67_CR1,0,45,67_AL_.jpg
